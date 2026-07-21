74967b8e654dce95544b4922d680fe7e4dfb8e55
Bowen Du <46970733+bowendu-nus@users.noreply.github.com>
2026-07-21 18:03:03 +0800

    Update __init__.py
    changed the order from alphabetical to the actual steps to follow

--- Airpeak/__init__.py
+++ Airpeak/__init__.py
@@ -1,22 +1,22 @@
-from .baseline_detection import baseline_detection
-from .dbscan import dbscan
-from .decay_regress import decay_regress
-from .feature_generation import feature_generation
-from .k_means_diff import k_means_diff
-from .k_means_ele import k_means_ele
 from .outlier_removal import outlier_removal
 from .smoothing import smoothing
+from .baseline_detection import baseline_detection
+from .feature_generation import feature_generation
+from .k_means_ele import k_means_ele
+from .k_means_diff import k_means_diff
+from .dbscan import dbscan
+from .decay_regress import decay_regress
 
 __all__ = [
-    "baseline_als",
-    "baseline_detection",
-    "dbscan",
-    "decay_regress",
-    "feature_generation",
-    "k_means_diff",
-    "k_means_ele",
     "outlier_removal",
     "smoothing",
+    "baseline_als",
+    "baseline_detection",
+    "feature_generation",
+    "k_means_ele",
+    "k_means_diff",
+    "dbscan",
+    "decay_regress",
 ]
 
 __version__ = "1.0.4"
